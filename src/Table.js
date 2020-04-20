6a998efa552e417db7bfcc833a22e9de12e04780
ric <ricardoptcosta@gmail.com>
2020-04-20 01:08:13 +0100

    added mock data and array element removal function

---
+++ src/Table.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+class Table extends Component {
+    render() {
+        const { characterData } = this.props
+
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} />
+            </table>
+        )
+    }
+}
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = (properties) => {
+    const rows = properties.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
+
+
+
+    return (
+        <tbody>
+            {rows}
+        </tbody>
+    )
+}
+
+
+
+export default Table
